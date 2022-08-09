06910cdb7d5f82d866c6bf355de723407a53a855
István Szmozsánszky <git@flaki.hu>
2022-08-09 22:37:17 +0000

    Overhaul CodeSandbox init script

--- csb-init.sh
+++ csb-init.sh
@@ -2,21 +2,22 @@
 # Initialize CodeSandbox project
 # ------------------------------
 
+cd /project/knockoff/suborbital
+
 # Docker runs in a high-uid namespace and won't be able to
 # create bind mounts and files unless we set these folders to
 # world-writable
-chmod o+w /project/knockoff/suborbital
-chmod o+w /project/knockoff/suborbital/config
+chmod o+w . ./config
 
+# The builder needs this file to exist
+touch .who
+
+# An SCN Environment token must be already saved to the CodeSandbox Env
 echo "SCC_ENV_TOKEN=$SCC_ENV_TOKEN" > /project/knockoff/suborbital/SCC.env
 
 # Download and install `subo` into a folder that's already on the $PATH
 # so we don't have to worry about updating the $PATH
-mkdir -p /project/home/flaki/.nix-profile/bin
-cd /project/home/flaki/.nix-profile/bin
-wget https://github.com/suborbital/subo/releases/latest/download/subo-v0.5.4-linux-amd64.tar.gz
-tar -xzf subo-v0.5.4-linux-amd64.tar.gz subo
-rm subo-v0.5.4-linux-amd64.tar.gz
+curl -L https://github.com/suborbital/subo/releases/latest/download/subo-v0.5.4-linux-amd64.tar.gz | tar -xzf - -C /usr/local/bin/ subo
 
 # ENV:
 # HOSTNAME=[xxxxxx]
