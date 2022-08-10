8dd5f37508715e31e661a90ac01434ea2f033b85
István Szmozsánszky <git@flaki.hu>
2022-08-10 16:26:31 +0000

    Use custom control plane to fix telemetry

--- csb-init.sh
+++ csb-init.sh
@@ -19,6 +19,13 @@ echo "SCC_ENV_TOKEN=$SCC_ENV_TOKEN" > /project/knockoff/suborbital/SCC.env
 # so we don't have to worry about updating the $PATH
 curl -L https://github.com/suborbital/subo/releases/latest/download/subo-v0.5.4-linux-amd64.tar.gz | tar -xzf - -C /usr/local/bin/ subo
 
+# Use a special version of the SCN Control Plane that is
+# immune to telemetry issues
+curl -s https://s3.flak.is/public/scc-heartbreak.tar.gz | gzip -dc - > scc-heartbreak.tar
+docker load -i scc-heartbreak.tar # => suborbital/scc-control-plane:heartbreak
+rm scc-heartbreak.tar
+
+
 # ENV:
 # HOSTNAME=[xxxxxx]
 # CODESANDBOX_HOST=[xxxxxx]-$PORT.preview.csb.app
